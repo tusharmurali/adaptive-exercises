e76cb5adbe365e1b11e427aa9251d0f206452547
Tushar Muralidharan <u7253634@anu.edu.au>
2023-04-16 16:39:21 +1000

    First commit

---
+++ textbooks.js
@@ -0,0 +1,240 @@
+const textbooks = [
+    {
+        id: "artin-algebra",
+        title: "Algebra",
+        authors: "Michael Artin",
+        chapters: [
+            {
+                number: 11,
+                name: 'Rings',
+                sections: [
+                    {
+                        number: 1,
+                        name: 'Definition of a Ring',
+                        exercises: 9
+                    },
+                    {
+                        number: 2,
+                        name: 'Polynomial Rings',
+                        exercises: 2
+                    },
+                    {
+                        number: 3,
+                        name: 'Homomorphisms and Ideals',
+                        exercises: 13
+                    },
+                    {
+                        number: 4,
+                        name: 'Quotient Rings',
+                        exercises: 4
+                    },
+                    {
+                        number: 5,
+                        name: 'Adjoining Elements',
+                        exercises: 7
+                    },
+                    {
+                        number: 6,
+                        name: 'Product Rings',
+                        exercises: 8
+                    },
+                    {
+                        number: 7,
+                        name: 'Fractions',
+                        exercises: 5
+                    },
+                    {
+                        number: 8,
+                        name: 'Maximal Ideals',
+                        exercises: 4
+                    },
+                    {
+                        number: 9,
+                        name: 'Algebraic Geometry',
+                        exercises: 13
+                    },
+                    {
+                        name: 'Miscellaneous',
+                        exercises: 7
+                    }
+                ]
+            },
+            {
+                number: 12,
+                name: 'Factoring',
+                sections: [
+                    {
+                        number: 1,
+                        name: 'Factoring Integers',
+                        exercises: 5
+                    },
+                    {
+                        number: 2,
+                        name: 'Unique Factorization Domains',
+                        exercises: 10
+                    },
+                    {
+                        number: 3,
+                        name: 'Gauss’s Lemma',
+                        exercises: 6
+                    },
+                    {
+                        number: 4,
+                        name: 'Factoring Integer Polynomials',
+                        exercises: 19
+                    },
+                    {
+                        number: 5,
+                        name: 'Gauss Primes',
+                        exercises: 10
+                    },
+                    {
+                        name: 'Miscellaneous',
+                        exercises: 11
+                    }
+                ]
+            },
+            {
+                number: 13,
+                name: 'Quadratic Number Fields',
+                sections: [
+                    {
+                        number: 1,
+                        name: 'Algebraic Integers',
+                        exercises: 4
+                    },
+                    {
+                        number: 2,
+                        name: 'Factoring Algebraic Integers',
+                        exercises: 2
+                    },
+                    {
+                        number: 3,
+                        name: 'Ideals in Z[√-5]',
+                        exercises: 4
+                    },
+                    {
+                        number: 4,
+                        name: 'Ideal Multiplication',
+                        exercises: 3
+                    },
+                    {
+                        number: 5,
+                        name: 'Factoring Ideals',
+                        exercises: 3
+                    },
+                    {
+                        number: 6,
+                        name: 'Prime Ideals and Prime Integers',
+                        exercises: 7
+                    },
+                    {
+                        number: 7,
+                        name: 'Ideal Classes',
+                        exercises: 4
+                    },
+                    {
+                        number: 8,
+                        name: 'Computing the Class Group',
+                        exercises: 6
+                    },
+                    {
+                        number: 9,
+                        name: 'Real Quadratic Fields',
+                        exercises: 5
+                    },
+                    {
+                        number: 10,
+                        name: 'About Lattices',
+                        exercises: 2
+                    },
+                    {
+                        name: 'Miscellaneous',
+                        exercises: 4
+                    }
+                ]
+            },
+            {
+                number: 15,
+                name: 'Fields',
+                sections: [
+                    {
+                        number: 1,
+                        name: 'Examples of Fields',
+                        exercises: 3
+                    },
+                    {
+                        number: 2,
+                        name: 'Algebraic and Transcendental Elements',
+                        exercises: 3
+                    },
+                    {
+                        number: 3,
+                        name: 'The Degree of a Field Extension',
+                        exercises: 10
+                    },
+                    {
+                        number: 4,
+                        name: 'Finding the Irreducible Polynomial',
+                        exercises: 3
+                    },
+                    {
+                        number: 5,
+                        name: 'Ruler and Compass Constructions',
+                        exercises: 6
+                    },
+                    {
+                        number: 6,
+                        name: 'Adjoining Roots',
+                        exercises: 3
+                    },
+                    {
+                        number: 7,
+                        name: 'Finite Fields',
+                        exercises: 14
+                    },
+                    {
+                        number: 8,
+                        name: 'Primitive Elements',
+                        exercises: 2
+                    },
+                    {
+                        number: 9,
+                        name: 'Function Fields',
+                        exercises: 5
+                    },
+                    {
+                        number: 10,
+                        name: 'The Fundamental Theorem of Algebra',
+                        exercises: 4
+                    },
+                    {
+                        name: 'Miscellaneous',
+                        exercises: 7
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        id: "stein-shakarchi-real-analysis",
+        title: "Real Analysis",
+        authors: "Elias M. Stein & Rami Shakarchi",
+        chapters: [
+            {
+                number: 1,
+                name: 'Measure Theory',
+                sections: [
+                    {
+                        name: 'Exercises',
+                        exercises: 39
+                    },
+                    {
+                        name: 'Problems',
+                        exercises: 8
+                    }
+                ]
+            }
+        ]
+    }
+];
